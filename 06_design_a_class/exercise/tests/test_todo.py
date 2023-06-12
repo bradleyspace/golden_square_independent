7a08d1e70ff9cd63ab1473630e6e83a120de10e8
Bradley <brad.holmes05@icloud.com>
2023-06-12 11:37:26 +0100

    create test_multiple tst

--- 06_design_a_class/exercise/tests/test_todo.py
+++ 06_design_a_class/exercise/tests/test_todo.py
@@ -1,5 +1,9 @@
 import pytest
+import random
 from lib.todo import TodoList, TodoNotFound
+import string
+
+LOWERCASE_LETTERS = string.ascii_letters
 
 def test_add_todo():
     instance = TodoList()
@@ -7,3 +11,16 @@ def test_add_todo():
 
     assert "My Todo!" == instance.get_by_id(_id)
 
+
+def test_multiple():
+    instance = TodoList()
+
+    strings = ["".join(random.choices(LOWERCASE_LETTERS, k=7)) for i in range(15)]
+
+    expected_items = []
+
+    for string in strings:
+        _id = instance.create(string)
+        expected_items.append((_id, string))
+
+    assert instance.get_all() == expected_items
